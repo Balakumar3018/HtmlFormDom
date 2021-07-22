3a4b60d07c402c4f10b57930084820a7328b71ff
Balakumar3018 <balakumarjkp@gmail.com>
2021-07-22 20:57:08 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -18,21 +18,47 @@
         let td3=document.querySelector("#Dadd1");
         td3.innerHTML=add1.value;
 
-        let add2=document.querySelector("#Addressline2");
-        let td4=document.querySelector("#Dadd2");
-        td4.innerHTML=add2.value;
+        let td4=document.querySelector("#Dgender");
+        Gender();
 
         let pin=document.querySelector("#pincode");
         let td5=document.querySelector("#Dpincode");
         td5.innerHTML=pin.value;
 
-        //let food=document.querySelector("#pincode");
+        let food=document.querySelector("#pincode");
+        let favfood=document.getElementsByName("favoritefood");
+        let td6=document.querySelector("#Dfavoritefood");
+        listfood  ();
+
         let state=document.querySelector("#state");
-        let td6=document.querySelector("#Dstate");
-        td6.innerHTML=state.value;
+        let td7=document.querySelector("#Dstate");
+        td7.innerHTML=state.value;
 
         let country=document.querySelector("#Country");
-        let td7=document.querySelector("#Dcountry");
-        td7.innerHTML=country.value;
+        let td8=document.querySelector("#Dcountry");
+        td8.innerHTML=country.value;
+
+        function Gender(){
+          if (document.querySelector('#male').checked) {
+            let male=document.querySelector("#male")
+            td4.innerHTML=male.value;
+          }else if(document.querySelector('#female').checked){
+            let female=document.querySelector("#female")
+            td4.innerHTML=female.value;
+          }
+          else{
+            td4.innerHTML='undefined';
+          }
+        }
+
+        function listfood(){
+          let temp=[];
+          for(let i=0;i<favfood.length;i++){
+              if(favfood[i].checked){
+                temp.push(favfood[i].value);
+              }
+          }
+          td6.innerHTML=temp;
+        }
        
     }
